dfd9795efca857c748a6e752f6eab959dbd7bef4
Lchenc05 <146626932+Lchenc05@users.noreply.github.com>
2025-02-11 13:43:23 +0100

    Añadido search

--- src/main/java/es/ucm/fdi/iw/controller/RootController.java
+++ src/main/java/es/ucm/fdi/iw/controller/RootController.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *  Non-authenticated requests only.
@@ -32,5 +33,11 @@ public class RootController {
     @GetMapping("/rewards")
     public String rewards(Model model) {
         return "rewards";
+    }
+
+   @GetMapping("/search")
+    public String search(@RequestParam(name = "username", required = false) String username, Model model) {
+        model.addAttribute("username", username);
+        return "search";
     }
 }
