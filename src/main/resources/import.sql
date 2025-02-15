35c84c8d8655e4b73e73086f8ab6251f5725720f
hwenj <146937278+hwenj@users.noreply.github.com>
2025-02-15 11:43:11 +0100

    login solucion

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,4 @@
+/*
 -- insert admin (username a, password aa)
 INSERT INTO IWUser (id, enabled, roles, username, password)
 VALUES (1, TRUE, 'ADMIN,USER', 'a',
@@ -8,3 +9,34 @@ VALUES (2, TRUE, 'USER', 'b',
 
 -- start id numbering from a value that is larger than any assigned above
 ALTER SEQUENCE "PUBLIC"."GEN" RESTART WITH 1024;
+*/
+
+-- Insertar admin (username a, password aa)
+INSERT INTO IWUser (id, enabled, roles, username, password, description, rating, pfp)
+VALUES (
+    1, 
+    TRUE, 
+    'ADMIN,USER', 
+    'a',
+    '{bcrypt}$2a$10$2BpNTbrsarbHjNsUWgzfNubJqBRf.0Vz9924nRSHBqlbPKerkgX.W',
+    'Usuario administrador', 
+    5.0, 
+    '/img/admin.png'
+);
+
+-- Insertar usuario normal (username b, password bb)
+INSERT INTO IWUser (id, enabled, roles, username, password, description, rating, pfp)
+VALUES (
+    2, 
+    TRUE, 
+    'USER', 
+    'b',
+    '{bcrypt}$2a$10$2BpNTbrsarbHjNsUWgzfNubJqBRf.0Vz9924nRSHBqlbPKerkgX.W',
+    'Usuario estándar', 
+    4.5, 
+    '/img/user.png'
+);
+
+-- Iniciar secuencia de IDs
+ALTER SEQUENCE GEN RESTART WITH 1024;
+
