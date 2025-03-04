bbee642b8f281ed913d2bc5db4b0843fe537e3ed
hwenj <146937278+hwenj@users.noreply.github.com>
2025-03-04 11:12:57 +0100

    fix index
    Co-Authored-By: Marco Antonio <110636189+DCCXXV@users.noreply.github.com>

--- src/main/java/es/ucm/fdi/iw/controller/RootController.java
+++ src/main/java/es/ucm/fdi/iw/controller/RootController.java
@@ -39,6 +39,16 @@ public class RootController {
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("actual", "inicio");
+        
+        List<User> recusers = UserService.getRecommendedUsers();
+        List<User> otherusers = UserService.getPopularUsers();
+        List<String> desiredSkills = SkillService.getRequestedSkills();
+        List<String> commonSkills = SkillService.getCommonSkills();
+
+        model.addAttribute("recusers", recusers);
+        model.addAttribute("otherusers", otherusers);
+        model.addAttribute("desiredSkills", desiredSkills);
+        model.addAttribute("commonSkills", commonSkills);
 
 		List<User> users = entityManager.createQuery("SELECT u FROM User u", User.class).getResultList();
 		model.addAttribute("users", users);
