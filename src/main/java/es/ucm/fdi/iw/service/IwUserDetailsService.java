a6d01e5ec4d5f505f5f7d0fb6e14793e26eee894
hwenj <146937278+hwenj@users.noreply.github.com>
2025-03-10 13:41:07 +0100

    fix vista user
    Co-Authored-By: Marco Antonio <110636189+DCCXXV@users.noreply.github.com>

--- src/main/java/es/ucm/fdi/iw/service/IwUserDetailsService.java
+++ src/main/java/es/ucm/fdi/iw/service/IwUserDetailsService.java
@@ -2,9 +2,6 @@ package es.ucm.fdi.iw.service;
 
 import java.util.ArrayList;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
